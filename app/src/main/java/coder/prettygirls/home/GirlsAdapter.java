1cee990e7b45c0277059b4ea542ee71ac1f99a99
renleicoder <1803474990@qq.com>
2016-08-01 18:40:09 +0800

    添加点击meizhi跳转到大图的动画

--- app/src/main/java/coder/prettygirls/home/GirlsAdapter.java
+++ app/src/main/java/coder/prettygirls/home/GirlsAdapter.java
@@ -1,6 +1,7 @@
 package coder.prettygirls.home;
 
 import android.content.Context;
+import android.view.View;
 import android.view.ViewGroup;
 
 import com.jude.easyrecyclerview.adapter.BaseViewHolder;
@@ -13,6 +14,8 @@ import coder.prettygirls.data.bean.GirlsBean;
  */
 public class GirlsAdapter extends RecyclerArrayAdapter<GirlsBean.ResultsEntity> {
 
+    public OnMyItemClickListener mOnItemClickListener;
+
     public GirlsAdapter(Context context) {
         super(context);
     }
@@ -20,5 +23,26 @@ public class GirlsAdapter extends RecyclerArrayAdapter<GirlsBean.ResultsEntity> 
     @Override
     public BaseViewHolder OnCreateViewHolder(ViewGroup parent, int viewType) {
         return new GirlsViewHolder(parent);
+    }
+
+    @Override
+    public void OnBindViewHolder(final BaseViewHolder holder, final int position) {
+        super.OnBindViewHolder(holder, position);
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mOnItemClickListener != null) {
+                    mOnItemClickListener.onItemClick(position, holder);
+                }
+            }
+        });
+    }
+
+    public interface OnMyItemClickListener {
+        void onItemClick(int position, BaseViewHolder holder);
+    }
+
+    public void setOnMyItemClickListener(OnMyItemClickListener listener) {
+        this.mOnItemClickListener = listener;
     }
 }
