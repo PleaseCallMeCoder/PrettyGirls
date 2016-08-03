2877676c2b393b38283e1062ec4ea4cb8f41cc7b
zhanglisan <zhanglisan@xianglin.cn>
2016-08-03 15:59:53 +0800

    add retrolambda library

--- app/src/main/java/coder/prettygirls/data/source/remote/RemoteGirlsDataSource.java
+++ app/src/main/java/coder/prettygirls/data/source/remote/RemoteGirlsDataSource.java
@@ -1,10 +1,8 @@
 package coder.prettygirls.data.source.remote;
 
-import coder.prettygirls.data.bean.GirlsBean;
 import coder.prettygirls.data.source.GirlsDataSource;
-import coder.prettygirls.http.GirlsRetrofit;
+import coder.prettygirls.http.RetrofitUtil;
 import coder.prettygirls.http.GirlsService;
-import rx.Observer;
 import rx.android.schedulers.AndroidSchedulers;
 import rx.schedulers.Schedulers;
 
@@ -15,27 +13,15 @@ public class RemoteGirlsDataSource implements GirlsDataSource {
 
     @Override
     public void getGirls(int page, int size, final LoadGirlsCallback callback) {
-        GirlsRetrofit.getRetrofit()
+        RetrofitUtil.getInstance()
                 .create(GirlsService.class)
                 .getGirls("福利", size, page)
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(new Observer<GirlsBean>() {
-                    @Override
-                    public void onCompleted() {
-
-                    }
-
-                    @Override
-                    public void onError(Throwable e) {
-                        callback.onDataNotAvailable();
-                    }
-
-                    @Override
-                    public void onNext(GirlsBean girlsBean) {
-                        callback.onGirlsLoaded(girlsBean);
-                    }
-                });
+                .subscribe(
+                        callback::onGirlsLoaded,
+                        throwable -> callback.onDataNotAvailable()
+                );
     }
 
     @Override
